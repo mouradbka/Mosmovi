4b2b357aa5e8105594152fb08b115b72a5835ec2
Mourad <mbkarib@gmail.com>
2022-02-01 14:23:05 +0100

    functions to process

--- utils/Processing.py
+++ utils/Processing.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import os
 
+def rename_coutries(x):
+    if x in ['Argentina','Argentine']:
+        return 'Argentina'
+    elif x in ['Brasile','Brasilien','Brazil','Brésil']:
+        return 'Brazil'
+    elif x in ['Chile','Chili']:
+        return 'Chile'
+    elif x in ['Fransk Guyana','French Guiana']:
+        return 'French Guiana'
+    else:
+        return x
+
 def format_csvs_to_df(path):
     file_names = os.listdir('../data/Copy of dev_south_america_merged/')
 
@@ -12,3 +24,4 @@ def format_csvs_to_df(path):
         frames.append(df)
     df_total = pd.concat(frames)
     return df_total
+
