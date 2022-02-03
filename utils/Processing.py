3815a091302d65549f42b5c4ba4ea4af0e09dd77
Mourad <mbkarib@gmail.com>
2022-02-03 16:29:13 +0100

    fixed some bugs in the processing

--- utils/Processing.py
+++ utils/Processing.py
@@ -14,12 +14,12 @@ def rename_coutries(x):
         return x
 
 def format_csvs_to_df(path):
-    file_names = os.listdir('../data/Copy of dev_south_america_merged/')
+    file_names = os.listdir(path)
 
     frames = []
     for n in file_names:
         coordinates = [float(x) for x in n[:-4].split('_')]
-        df = pd.read_csv('../data/Copy of dev_south_america_merged/' + n, sep=';')
+        df = pd.read_csv(path + n, sep=';')
         df['coordinates'] = [coordinates] * len(df)
         frames.append(df)
     df_total = pd.concat(frames)
