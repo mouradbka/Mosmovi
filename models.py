4a8c2ea4c471d3e43d5dc82179818d0752587187
vin <vinit.ravishankar@gmail.com>
2022-02-08 00:29:03 +0530

    fixes

--- models.py
+++ models.py
@@ -37,6 +37,7 @@ class CharCNNModel(nn.Module):
     def __init__(self, args):
         super(CharCNNModel, self).__init__()
         self._token_embed = nn.Embedding(256, 300, 255)
+
         self._conv1 = nn.Sequential(
             nn.Conv1d(300, 256, kernel_size=7, stride=1),
             nn.ReLU(),
@@ -62,32 +63,22 @@ class CharCNNModel(nn.Module):
         self._conv6 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride=3)
+            nn.AdaptiveMaxPool1d(output_size=1)
         )
-        self._fc1 = nn.Sequential(
-            nn.Linear(8704, 1024),
-            nn.ReLU(),
-            nn.Dropout(p=args.dropout)
-        )
-        self._fc2 = nn.Sequential(
-            nn.Linear(1024, 1024),
-            nn.ReLU(),
-            nn.Dropout(p=args.dropout)
-        )
+        self._fc1 = nn.Sequential(nn.Linear(256, 1024), nn.ReLU(), nn.Dropout(p=args.dropout))
+        self._fc2 = nn.Sequential(nn.Linear(1024, 1024), nn.ReLU(), nn.Dropout(p=args.dropout))
         self._fc3 = nn.Linear(1024, 2)
 
     def forward(self, x):
         x = self._token_embed(x)
-        #transpose
-        x = x.permute(0,2,1)
+        # transpose
+        x = x.permute(0, 2, 1)
         x = self._conv1(x)
         x = self._conv2(x)
         x = self._conv3(x)
         x = self._conv4(x)
         x = self._conv5(x)
-        x = self._conv6(x)
-        # collapse
-        x = x.view(x.size(0), -1)
+        x = self._conv6(x).squeeze()
         # linear layer
         x = self._fc1(x)
         # linear layer
