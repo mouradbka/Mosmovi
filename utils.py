c7e2d08a982c20c599562521fd2361cb7eb0f254
vin <vinit.ravishankar@gmail.com>
2022-02-11 16:22:22 +0530

    rollback

--- utils.py
+++ utils.py
@@ -42,11 +42,6 @@ def gc_distance(gold, pred):
     sin_gold = torch.sin(rad_gold)
     cos_pred = torch.cos(rad_pred)
     sin_pred = torch.sin(rad_pred)
-    #expand pred dims when batch size = 1
-    if len(sin_pred) == 1:
-        cos_pred.unsqueeze()
-        sin_pred.unsqueeze()
-
 
     n_gold = torch.stack([cos_gold[:, 0] * cos_gold[:, 1], cos_gold[:, 0] * sin_gold[:, 1], sin_gold[:, 0]], dim=1)
     n_pred = torch.stack([cos_pred[:, 0] * cos_pred[:, 1], cos_pred[:, 0] * sin_pred[:, 1], sin_pred[:, 0]], dim=1)
