71279ee85004d81b6b3442c76fee7e9906374833
vqc439 <vqc439@a00552.science.domain>
2022-02-28 15:52:04 +0100

    merge

--- model_utils.py
+++ model_utils.py
@@ -67,16 +67,16 @@ def gaussian_probability(sigma, mu, target):
     """
     target = target.unsqueeze(1).expand_as(sigma)
     ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((target - mu) / sigma)**2) / sigma
-    return ret #torch.prod(ret, 2)
+    return torch.mean(ret, 2)
 
-#def mdn_loss(pi, sigma, mu, target):
-#    """Calculates the error, given the MoG parameters and the target
-#    The loss is the negative log likelihood of the data given the MoG
-#    parameters.
-#    """
-#    prob = pi * gaussian_probability(sigma, mu, target)
-#    nll = -torch.log(torch.sum(prob, dim=1))
-#    return torch.mean(nll)
+def mdn_loss(pi, sigma, mu, target):
+    """Calculates the error, given the MoG parameters and the target
+    The loss is the negative log likelihood of the data given the MoG
+    parameters.
+    """
+    prob = pi * gaussian_probability(sigma, mu, target)
+    nll = -torch.log(torch.sum(prob, dim=1))
+    return torch.mean(nll)
 
 def mdn_log_prob(pi, sigma, mu, y, temp=1):
     log_component_prob = gaussian_probability(sigma, mu, y)
@@ -85,37 +85,37 @@ def mdn_log_prob(pi, sigma, mu, y, temp=1):
                 pi, tau=temp, dim=1) + 1e-15).unsqueeze(-1).repeat(1,1,2)
     return torch.logsumexp(log_component_prob + log_mix_prob, dim=-1)
 
-def mdn_loss(pi, sigma, mu, y):
-    # NLL Loss
-    log_prob = mdn_log_prob(pi, sigma, mu, y)
-    loss = torch.mean(-log_prob)
-    """
-    if self.hparams.mdn_config.weight_regularization is not None:
-        sigma_l1_reg = 0
-        pi_l1_reg = 0
-        mu_l1_reg = 0
-        if self.hparams.mdn_config.lambda_sigma > 0:
-            # Weight Regularization Sigma
-            sigma_params = torch.cat(
-                [x.view(-1) for x in self.mdn.sigma.parameters()]
-            )
-            sigma_l1_reg = self.hparams.mdn_config.lambda_sigma * torch.norm(
-                sigma_params, self.hparams.mdn_config.weight_regularization
-            )
-        if self.hparams.mdn_config.lambda_pi > 0:
-            pi_params = torch.cat([x.view(-1) for x in self.mdn.pi.parameters()])
-            pi_l1_reg = self.hparams.mdn_config.lambda_pi * torch.norm(
-                pi_params, self.hparams.mdn_config.weight_regularization
-            )
-        if self.hparams.mdn_config.lambda_mu > 0:
-            mu_params = torch.cat([x.view(-1) for x in self.mdn.mu.parameters()])
-            mu_l1_reg = self.hparams.mdn_config.lambda_mu * torch.norm(
-                mu_params, self.hparams.mdn_config.weight_regularization
-            )
-    """
-    loss = loss #+ sigma_l1_reg + pi_l1_reg + mu_l1_reg
-    return loss
-
+# def mdn_loss(pi, sigma, mu, y):
+#     # NLL Loss
+#     log_prob = mdn_log_prob(pi, sigma, mu, y)
+#     loss = torch.mean(-log_prob)
+#     """
+#     if self.hparams.mdn_config.weight_regularization is not None:
+#         sigma_l1_reg = 0
+#         pi_l1_reg = 0
+#         mu_l1_reg = 0
+#         if self.hparams.mdn_config.lambda_sigma > 0:
+#             # Weight Regularization Sigma
+#             sigma_params = torch.cat(
+#                 [x.view(-1) for x in self.mdn.sigma.parameters()]
+#             )
+#             sigma_l1_reg = self.hparams.mdn_config.lambda_sigma * torch.norm(
+#                 sigma_params, self.hparams.mdn_config.weight_regularization
+#             )
+#         if self.hparams.mdn_config.lambda_pi > 0:
+#             pi_params = torch.cat([x.view(-1) for x in self.mdn.pi.parameters()])
+#             pi_l1_reg = self.hparams.mdn_config.lambda_pi * torch.norm(
+#                 pi_params, self.hparams.mdn_config.weight_regularization
+#             )
+#         if self.hparams.mdn_config.lambda_mu > 0:
+#             mu_params = torch.cat([x.view(-1) for x in self.mdn.mu.parameters()])
+#             mu_l1_reg = self.hparams.mdn_config.lambda_mu * torch.norm(
+#                 mu_params, self.hparams.mdn_config.weight_regularization
+#             )
+#     """
+#     loss = loss #+ sigma_l1_reg + pi_l1_reg + mu_l1_reg
+#     return loss
+#
 
 
 def sample(pi, sigma, mu):
@@ -133,35 +133,3 @@ def sample(pi, sigma, mu):
     return (gaussian_noise * variance_samples + mean_samples).transpose(0, 1)
 
 
-def sample(pi, sigma, mu):
-    """Draw samples from a MoG."""
-    pis = Categorical(pi).sample().view(pi.size(0), 1, 1)
-
-    sample = Variable(sigma.data.new(sigma.size(0), 1).normal_())
-    # Gathering from the n Gaussian Distribution based on sampled indices
-    sample = sample * sigma.gather(1, pis) + mu.gather(1, pis)
-    return sample
-
-def generate_samples(pi, sigma, mu, n_samples=1000):
-    samples = []
-    softmax_pi = nn.functional.gumbel_softmax(
-        pi, tau=1.0, dim=-1
-    ).unsqueeze(-1).repeat(1,1,2)
-
-    assert (
-        softmax_pi < 0
-    ).sum().item() == 0, "pi parameter should not have negative"
-
-    for _ in range(n_samples):
-        samples.append(sample(softmax_pi, sigma, mu))
-    samples = torch.cat(samples, dim=1)
-    return samples
-
-def generate_point_predictions(self, pi, sigma, mu, n_samples=None):
-    # Sample using n_samples and take average
-    samples = self.generate_samples(pi, sigma, mu, n_samples)
-    #if central_tendency == "mean":
-    y_hat = torch.mean(samples, dim=-1)
-    #elif central_tendency == "median":
-    #y_hat = torch.median(samples, dim=-1).values
-    return y_hat.unsqueeze(1)
