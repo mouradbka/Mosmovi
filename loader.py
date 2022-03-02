1b7019f5f081327e3eee242122ca0934927feaa3
vin <vinit.ravishankar@gmail.com>
2022-03-02 12:03:06 +0100

    fname

--- loader.py
+++ loader.py
@@ -13,6 +13,7 @@ class TweetDataset(Dataset):
         self.tweet_time = []
         self.author_time = []
         self.author_desc = []
+        self.fnames = []
         self.coords = []
         self.uids = []
         self.char_max_length = char_max_length
@@ -20,6 +21,7 @@ class TweetDataset(Dataset):
 
         for fname in glob.glob(f"{data_dir}/*"):
             df = pd.read_csv(fname, sep=';', header=0)
+            self.fnames.append(fname)
             self.tweet_tokens.extend([str(i) for i in df.text.tolist()])
             self.uids.extend(df.author_id.tolist())
             self.coords.extend([tuple(map(float, fname.rstrip('.csv').split('/')[-1].split('_')))] * len(df))
@@ -44,9 +46,10 @@ class TweetDataset(Dataset):
         if self.use_metadata:
             # tweet_time = torch.nan_to_num(torch.FloatTensor([self.tweet_time[idx]]), 0.5)
             # author_time = torch.nan_to_num(torch.FloatTensor([self.author_time[idx]]), 0.5)
+            fname = self.fnames[idx]
             tweet_time = torch.FloatTensor([self.tweet_time[idx]])
             author_time = torch.FloatTensor([self.author_time[idx]])
             author_desc = self.author_desc[idx]
-            metadata = (tweet_time, author_time, author_desc)
+            metadata = (tweet_time, author_time, author_desc, fname)
 
         return tokens, coords, metadata
