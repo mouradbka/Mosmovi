b95d95ae62b435a24bffc6efe1b152cf5907bff9
vin <vinit.ravishankar@gmail.com>
2022-03-03 12:12:26 +0100

    fix

--- debug_dataset.py
+++ debug_dataset.py
@@ -11,14 +11,19 @@ logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--data_dir', default='./data', action='store')
+    parser.add_argument('--data_dir', default='./data_full', action='store')
     args = parser.parse_args()
+    fail_files = []
+    fails = []
     tweet_dataset = TweetDataset(data_dir=args.data_dir, use_metadata=True)
     print(len(tweet_dataset))
     for i in tqdm.tqdm(tweet_dataset):
-        times = i[2][0]
-        if math.isnan(times):
-            print(i)
+        if math.isnan(i[2][0]) or math.isnan(i[2][1]):
+        # if times != times:
+        #     fails.append(i[2][2])
+            fail_files.append(i[2][-1])
+
+    print(set(fail_files))
 
 
 if __name__ == '__main__':
--- loader.py
+++ loader.py
@@ -23,7 +23,7 @@ class TweetDataset(Dataset):
 
         for fname in glob.glob(f"{data_dir}/*"):
             df = pd.read_csv(fname, sep=';', header=0)
-            self.fnames.append(fname)
+            self.fnames.extend([fname] * len(df))
             self.tweet_tokens.extend([str(i) for i in df.text.tolist()])
             self.uids.extend(df.author_id.tolist())
             self.coords.extend([tuple(map(float, fname.rstrip('.csv').split('/')[-1].split('_')))] * len(df))
@@ -62,11 +62,9 @@ class TweetDataset(Dataset):
             labels = torch.FloatTensor(self.coords[idx])
         metadata = None
         if self.use_metadata:
-            # tweet_time = torch.nan_to_num(torch.FloatTensor([self.tweet_time[idx]]), 0.5)
-            # author_time = torch.nan_to_num(torch.FloatTensor([self.author_time[idx]]), 0.5)
+            tweet_time = torch.nan_to_num(torch.FloatTensor([self.tweet_time[idx]]), 0.5)
+            author_time = torch.nan_to_num(torch.FloatTensor([self.author_time[idx]]), 0.5)
             fname = self.fnames[idx]
-            tweet_time = torch.FloatTensor([self.tweet_time[idx]])
-            author_time = torch.FloatTensor([self.author_time[idx]])
             author_desc = self.author_desc[idx]
             metadata = (tweet_time, author_time, author_desc, fname)
 
