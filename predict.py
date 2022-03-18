e03ebcf102a296bb916c1d198043f265d677f7f2
mhany90 <mostafahany56@gmail.com>
2022-03-18 16:43:38 +0100

    predict with mdn

--- predict.py
+++ predict.py
@@ -5,6 +5,7 @@ import utils
 import tqdm
 import torch
 import torch.nn.functional as F
+from collections import defaultdict
 
 from argparse import ArgumentParser
 from torch import nn
@@ -26,6 +27,11 @@ def main():
     parser.add_argument('--model_path', required=True, type=str, action='store')
     parser.add_argument('--data_dir', required=True, type=str, action='store')
     parser.add_argument('--generate', action='store_true')
+    parser.add_argument('--mdn', action='store_true', default=False)
+    parser.add_argument('--use_mixture', action='store_true', default=False)
+    parser.add_argument('--num_confidence_bins', type=int, default=5)
+    parser.add_argument('--entropy_confidence', action='store_true', default=False)
+
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -50,11 +56,37 @@ def main():
 
     with torch.no_grad():
         distances = []
+        distances_confidence = defaultdict(list)
+
         for batch in test_iter:
-            test_loss, test_distance = utils.evaluate(batch, model, criterion, device=device, generate=args.generate)
+            #test_loss, test_distance = utils.evaluate(batch, model, criterion, device=device, generate=args.generate)
+            eval_stats = utils.evaluate(batch, model, criterion, args.mdn,
+                                        device=device,
+                                        mdn_mixture=args.use_mixture,
+                                        no_bins=args.num_confidence_bins,
+                                        entropy_confidence=args.entropy_confidence,
+                                        generate = args.generate)
+            if args.mdn:
+                test_loss, test_distance, test_distance_confidence = eval_stats
+                for confidence_level, corresp_test_distance in test_distance_confidence.items():
+                    distances_confidence[int(confidence_level)].extend(corresp_test_distance.tolist())
+
+            else:
+                test_loss, test_distance = eval_stats
+
+
             test_iter.set_description(f"test loss: {test_loss.item()}")
             distances.extend(test_distance.tolist())
         logger.info(f"test_mean: {np.mean(distances)}, test_median: {np.median(distances)}")
+
+        if args.mdn:
+            for confidence_level, corresp_test_distance_list in distances_confidence.items():
+                test_mean_c = np.nan_to_num(np.mean(corresp_test_distance_list))
+                test_median_c = np.nan_to_num(np.median(corresp_test_distance_list))
+                wandb.log({"conf: " + str(confidence_level) + " - " + "val_mean": test_mean_c,
+                           "conf: " + str(confidence_level) + " - " + "val_median": test_median_c})
+                logger.info(f"conf: " + str(confidence_level) + " - " + f"val_mean: {test_mean_c}")
+                logger.info(f"conf: " + str(confidence_level) + " - " + f"val_median: {test_median_c}")
 
 
 if __name__ == '__main__':
