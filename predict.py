dd720e3bc060ae13d24748ba53913b67fd3d96d5
mhany90 <mostafahany56@gmail.com>
2022-03-18 16:46:43 +0100

    add use metadata

--- predict.py
+++ predict.py
@@ -27,6 +27,7 @@ def main():
     parser.add_argument('--model_path', required=True, type=str, action='store')
     parser.add_argument('--data_dir', required=True, type=str, action='store')
     parser.add_argument('--generate', action='store_true')
+    parser.add_argument('--use_metadata', action='store_true')
     parser.add_argument('--mdn', action='store_true', default=False)
     parser.add_argument('--use_mixture', action='store_true', default=False)
     parser.add_argument('--num_confidence_bins', type=int, default=5)
